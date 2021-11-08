d01e517033f296b569e886987c689206145804e4
LiorA <lior@example.com>
2021-11-08 23:31:18 +0200

    converted to tensors

--- PyTorchFuzzyART.py
+++ PyTorchFuzzyART.py
@@ -18,12 +18,12 @@ class FuzzyArt:
 
     def train(self, pattern: np.array):
          # check that pattern is the correct length
-        if pattern.size != self.pattern_size:
+        if len(pattern[0]) != self.pattern_size:
             log.warn("input was the wrong size")
             return None
 
         # create the complemnet 
-        pattern = np.concatenate((pattern, 1 - pattern), axis = 1)
+        pattern = torch.cat((pattern, 1 - pattern), axis = 1)
 
        # select the winner category and learn the pattern with it
         J = self.choose_category(pattern)
@@ -34,8 +34,8 @@ class FuzzyArt:
 
     def choose_category(self, pattern: np.array):
         N = len(self.categories)
-        memberships = np.zeros(N)
-        choices = np.zeros(N)
+        memberships = torch.zeros(N)
+        choices = torch.zeros(N)
 
         # find the choice for each category
         for j in range(0, N):
@@ -49,16 +49,16 @@ class FuzzyArt:
 
         # iterate through categories by descending choice
         # until we find one that meets vigilance criteria
-        pattern_norm = pattern.sum()
-        order = np.argsort(choices)
+        # pattern_norm = pattern.sum() -> this is always pattern_size due to complementary normalization
+        order = torch.argsort(choices)
         for i in range(0, len(order)):
             j = order[i]
-            match = memberships[j] / pattern_norm
+            match = memberships[j] / self.pattern_size
             if match >= self.vigilance: return j
 
         # none of the categories matched
         # add a new one
-        self.categories.append(np.ones(self.complemented_pattern_size))
+        self.categories.append(torch.ones(self.complemented_pattern_size))
         self.category_counts.append(0)
         self.category_committed.append(False)
         return N
@@ -67,28 +67,28 @@ class FuzzyArt:
         category = self.categories[J]
         # Fast commit slow recode eq.8 in paper https://doi.org/10.1016/0893-6080(91)90056-B
         if self.category_committed[J]:
-            category = self.learn_rate * np.minimum(pattern, category) + (1 - self.learn_rate) * category
+            category = self.learn_rate * torch.minimum(pattern, category) + (1 - self.learn_rate) * category
         else:
-            category = np.minimum(pattern, category)
+            category = torch.minimum(pattern, category)
             self.category_committed[J] = True
         self.categories[J] = category
         self.category_counts[J] += 1
 
-def complement_code(x: np.array):
-    return np.concatenate((x, 1 - x), axis = 1)
+def complement_code(x: torch.Tensor):
+    return torch.cat((x, 1 - x), axis = 1)
 
 if __name__ == "__main__":
     # instantiate the ART module
     art = FuzzyArt(
-        pattern_size=5,
+        pattern_size=2,
         vigilance=0.9,
         choice=0.9,
         learn_rate=0.1
     )
 
     # present some random patterns
-    for i in range(0, 200):
-        pattern = np.array(np.random.rand(1, art.pattern_size))  # input patter with size (1,pattern_size)
+    for i in range(0, 20):
+        pattern = torch.tensor(np.array(np.random.rand(1, art.pattern_size)))  # input patter with size (1,pattern_size)
         #pattern = p1
         pattern_coded = complement_code(pattern)
         label = art.train(pattern)
